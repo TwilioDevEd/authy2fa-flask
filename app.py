4ccab8ea15d549645b492f55a311ca40361732f0
Matt Makai <makaimc@gmail.com>
2015-02-19 12:15:52 -0500

    adding psotgresql dependencies

--- app.py
+++ app.py
@@ -1,16 +1,40 @@
 import os
 from flask import Flask, render_template
+from flask.ext.sqlalchemy import SQLAlchemy
 
 
 ACCOUNT_SID = os.environ.get('TWILIO_ACCOUNT_SID', '')
 AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN', '')
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '')
+
 
 app = Flask(__name__, static_path="")
+db = SQLAlchemy(app)
 
 
 @app.route('/')
-def signin():
+def home():
     return render_template("index.html")
+
+
+@app.route('/user', methods=['POST'])
+def signup():
+    return 'ok'
+
+
+@app.route('/session', methods=['POST'])
+def session():
+    return 'ok'
+
+
+@app.route('/session/verify', methods=['POST'])
+def verify():
+    return 'ok'
+
+
+@app.route('/session/resend', methods=['POST'])
+def resend():
+    return 'ok'
 
 
 if __name__ == '__main__':
