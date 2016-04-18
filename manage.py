fbc591d3efae2906a27bbce3e1c494ed43a846b4
Victor Shyba <victor.shyba@gmail.com>
2016-04-18 12:38:57 -0300

    Fixes a bug on test loader
    As suggested by @dprothero
    Closes #7

--- manage.py
+++ manage.py
@@ -22,9 +22,12 @@ manager.add_command('db', MigrateCommand)
 @manager.command
 def test():
     """Run the unit tests."""
-    import unittest
+    import sys, unittest
     tests = unittest.TestLoader().discover('tests')
-    unittest.TextTestRunner(verbosity=2).run(tests)
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+
+    if not result.wasSuccessful():
+        sys.exit(1)
 
 if __name__ == "__main__":
     manager.run()
